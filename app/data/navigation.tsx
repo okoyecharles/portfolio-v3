b63ca4a992429d9970a4424f4742ef9198f4db59
Charles <okoyecharles509@gmail.com>
2023-12-18 09:30:57 +0100

    chore: store data about navigation

---
+++ app/data/navigation.tsx
@@ -0,0 +1,47 @@
+import GithubIcon from "../components/svg/GithubIcon";
+import LinkedinIcon from "../components/svg/LinkedinIcon";
+import XIcon from "../components/svg/XIcon";
+
+interface navData {
+  anchors: { name: string; link: string }[];
+  socials: { name: string; icon: React.JSX.Element; link: string }[];
+}
+
+const navigationData: navData = {
+  anchors: [
+    {
+      name: "home",
+      link: "#",
+    },
+    {
+      name: "about",
+      link: "#",
+    },
+    {
+      name: "projects",
+      link: "#",
+    },
+    {
+      name: "contact me",
+      link: "#",
+    },
+  ],
+  socials: [
+    {
+      name: "github",
+      icon: <GithubIcon />,
+      link: "https://github.com/okoyecharles",
+    },
+    {
+      name: "linkedin",
+      icon: <LinkedinIcon />,
+      link: "https://linkedin.com/in/okoyecharles",
+    },
+    {
+      name: "x",
+      icon: <XIcon />,
+      link: "https://x.com/okoyecharlesk",
+    },
+  ],
+};
+export default navigationData;
