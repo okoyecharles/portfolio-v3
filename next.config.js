f8ed02cc39b3115ea5ebfc75cec35fbd135c8548
Charles <okoyecharles509@gmail.com>
2023-12-19 08:50:11 +0100

    feat: add pwa (progressive web app) functionality

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
-module.exports = nextConfig
+module.exports = withPWA({});
