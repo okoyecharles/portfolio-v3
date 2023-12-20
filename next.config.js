40cfb68a67748a3e821db98ab381477647144813
Charles <okoyecharles509@gmail.com>
2023-12-20 13:35:03 +0100

    refactor: refactor next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reactStrictMode: true, // Enable React strict mode for improved error handling
+  // swcMinify: true,      // Enable SWC minification for improved performance
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  // },
+};
+
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development"
 });
 
-module.exports = withPWA({});
+module.exports = withPWA(nextConfig);
