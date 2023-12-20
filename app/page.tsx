2ccb67dd6a1ce10bc7d09837ec7c0a46bf4b5a88
Charles <okoyecharles509@gmail.com>
2023-12-20 16:30:01 +0100

    feat: create home background component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Footer from "./components/footer/Footer";
 import Navigation from "./components/navigation/Navigation";
+import HomeBackground from "./components/sections/home/HomeBackground";
 
 export default function Page() {
   return (
     <>
       <Navigation />
-      <main>
-        <span className="font-visby font-extrabold">Hello World</span>
+      <main className="h-full  p-6 text-grey-9 flex justify-center relative">
+        <HomeBackground />
       </main>
       <Footer />
     </>
