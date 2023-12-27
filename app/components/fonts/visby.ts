521aba78ec3b40048954cbf613fbf8c0d2791162
Charles <okoyecharles509@gmail.com>
2023-12-27 18:48:35 +0100

    chore: change visby cf font from .woff2 to .otf

--- app/components/fonts/visby.ts
+++ app/components/fonts/visby.ts
@@ -3,72 +3,72 @@ import localFont from "next/font/local";
 const visby = localFont({
   src: [
     {
-      path: "../../../public/fonts/visby/Visby-Thin.woff2",
+      path: "../../../public/fonts/visby/Visby-Thin.otf",
       weight: "100",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Thin-Italic.woff2",
+      path: "../../../public/fonts/visby/Visby-ThinItalic.otf",
       weight: "100",
       style: "italic",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Light.woff2",
+      path: "../../../public/fonts/visby/Visby-Light.otf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Light-Italic.woff2",
+      path: "../../../public/fonts/visby/Visby-LightItalic.otf",
       weight: "300",
       style: "italic",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Regular.woff2",
+      path: "../../../public/fonts/visby/Visby-Regular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Regular-Italic.woff2",
+      path: "../../../public/fonts/visby/Visby-RegularItalic.otf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Medium.woff2",
+      path: "../../../public/fonts/visby/Visby-Medium.otf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Medium-Italic.woff2",
+      path: "../../../public/fonts/visby/Visby-MediumItalic.otf",
       weight: "500",
       style: "italic",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Demibold.woff2",
+      path: "../../../public/fonts/visby/Visby-Demibold.otf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Demibold-Italic.woff2",
+      path: "../../../public/fonts/visby/Visby-DemiboldItalic.otf",
       weight: "600",
       style: "italic",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Bold.woff2",
+      path: "../../../public/fonts/visby/Visby-Bold.otf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Bold-Italic.woff2",
+      path: "../../../public/fonts/visby/Visby-BoldItalic.otf",
       weight: "700",
       style: "italic",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Extrabold.woff2",
+      path: "../../../public/fonts/visby/Visby-Extrabold.otf",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/visby/Visby-Extrabold-Italic.woff2",
+      path: "../../../public/fonts/visby/Visby-ExtraboldItalic.otf",
       weight: "800",
       style: "italic",
     },
--- app/components/sections/home/Home.tsx
+++ app/components/sections/home/Home.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="font-extrabold text-grey-2 dark:text-grey-b text-[24px] font-mono absolute leading-[1] -top-6 -left-0 md:-top-2 lg:-top-0 md:-left-10 select-none">
           I<span className="text-blue-200 dark:text-blue-d-200">'</span>m
         </div>
-        <h3 className="uppercase dark:text-grey-6 font-visby font-semibold spafi md:text-[18px] md:text-center tracking-wide">I embrace the digital world</h3>
+        <h3 className="uppercase dark:text-grey-6 font-lato font-semibold md:text-[18px] md:text-center">I embrace the digital world</h3>
       </header>
       <p className="py-9 max-w-[700px] md:text-[18px] md:text-center md:self-center">
         I can help you build a product, feature, or website. Look through my
