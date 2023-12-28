a9e379394b5fba33688e908ab6f8470bcff3d8db
Charles <okoyecharles509@gmail.com>
2023-12-28 08:47:16 +0100

    feat: animate in/out navigation based on scroll direction

--- app/components/navigation/DesktopNavLinks.tsx
+++ app/components/navigation/DesktopNavLinks.tsx
@@ -2,6 +2,7 @@
 import VerticalLineIcon from "../svg/VerticalLineIcon";
 import navigationData from "../../data/navigation";
 import { a, to, useSpring } from "@react-spring/web";
+import useScrollDirection from "../utils/useScrollDirection";
 
 export default function DesktopNavLinks({ active }: { active: number }) {
   const activeMarkerMorph = [
@@ -16,9 +17,23 @@ export default function DesktopNavLinks({ active }: { active: number }) {
       x: activeMarkerMorph[active].pos,
     },
   });
+  
+  const scrollDirection = useScrollDirection();
+  const activeNavSpring = useSpring({
+    from: { y: 0 },
+    to: {
+      y: scrollDirection == 'down' ? -100 : 0,
+    },
+    delay: 250,
+    config: {
+      tension: 300,
+      friction: 40,
+    }
+  });
 
   return (
-    <nav className="absolute w-fit top-9 right-9 bg-white dark:bg-grey-2 ring-1 dark:ring-0 ring-grey-ea rounded-[10px] font-visby font-medium px-6 py-[13px] gap-6 hidden md:flex items-center overflow-hidden select-none">
+    <a.nav className="absolute w-fit top-9 right-9 bg-white dark:bg-grey-2 ring-1 dark:ring-0 ring-grey-ea rounded-[10px] font-visby font-medium px-6 py-[13px] gap-6 hidden md:flex items-center overflow-hidden select-none" style={activeNavSpring}>
+      {/* { scrollDirection } */}
       <a.div
         className="active-marker absolute left-0 bottom-0 translate-y-1/2 h-[10px] bg-blue-100 dark:bg-blue-d-200 rounded-[4px]"
         style={{
@@ -53,6 +68,6 @@ export default function DesktopNavLinks({ active }: { active: number }) {
           </li>
         ))}
       </ul>
-    </nav>
+    </a.nav>
   );
 }
--- app/components/navigation/Navigation.tsx
+++ app/components/navigation/Navigation.tsx
@@ -3,14 +3,13 @@ import DesktopNavLinks from "./DesktopNavLinks";
 import LogoIcon from "../svg/Logo";
 
 export default function Navigation() {
-  const activeNavigationLink = 3;
+  const activeNavigationLink = 0;
+
   return (
     <>
       <div className="navigation-container max-w-[1504px] sticky top-0 mx-auto z-10">
-        <div className="relative">
-          <NavLinks active={activeNavigationLink} />
-          <DesktopNavLinks active={activeNavigationLink} />
-        </div>
+        <NavLinks active={activeNavigationLink} />
+        <DesktopNavLinks active={activeNavigationLink} />
       </div>
       <header className="p-6 md:p-8 w-full max-w-[1504px] mx-auto">
         <div className="logo h-12 flex items-center w-fit">
