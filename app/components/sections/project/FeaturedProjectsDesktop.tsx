b679b223269547dfbb88ac0c83e3b13f6b3369f9
Charles <okoyecharles509@gmail.com>
2024-01-14 22:27:10 +0100

    chore: isolate featured project tag component

--- app/components/sections/project/FeaturedProjectsDesktop.tsx
+++ app/components/sections/project/FeaturedProjectsDesktop.tsx
@@ -26,6 +26,7 @@ import animation from "../../animations/animations";
 import HorizontalDottedLine from "../../background/HorizontalDottedLine";
 import DottedLine from "../../background/DottedLine";
 import Plus from "../../background/Plus";
+import FeaturedProjectTag from "@/app/components/sections/project/FeaturedProjectsTag";
 
 export default function FeaturedProjectsDesktop(props: FeaturedProjectProps) {
   const { projectIndex, projects } = props;
@@ -330,14 +331,3 @@ function FeaturedProjectList({
   );
 }
 
-function FeaturedProjectTag({ name }: FeaturedProjectTagProps) {
-  const color = `text-tag-${name} dark:text-tag-${name}-dark`;
-  const bg = `bg-tag-${name}/20 dark:bg-tag-${name}-dark/25`;
-  return (
-    <span
-      className={`${color} ${bg} text-[14px] rounded-[11px] leading-[1] py-1 px-2`}
-    >
-      {name}
-    </span>
-  );
-}
