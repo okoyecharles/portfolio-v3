09848ed0d82caad2df7979bd4c118d6ae2bdf0e8
Charles <okoyecharles509@gmail.com>
2024-01-29 18:55:28 +0100

    refactor: add control to recommendation component

--- app/components/sections/recommendation/Recommendation.tsx
+++ app/components/sections/recommendation/Recommendation.tsx
@@ -5,28 +5,37 @@ import SectionHeader from "../SectionHeader";
 import recommendationData from "@/app/data/recommendation";
 import { useState } from "react";
 import RecommendationSwiper from "./RecommendationSwiper";
+import RecommendationControl from "./RecommendationControl";
 
 export default function Recommendation() {
   const recommedations = recommendationData;
   const [recommedationIndex, setRecommedationIndex] = useState<number>(2);
 
   return (
-    <Section name="recommendations" id="recommendations">
+    <Section
+      name="recommendations"
+      id="recommendations"
+      padding="pt-12 pb-16 md:py-8 md:pb-[224px]"
+    >
       <SectionHeader mode="standalone">
         Collaborating{" "}
         <span className="text-blue-100 dark:blue-d-200">remotely</span> with
-        developers across the globe
+        professionals across the globe
       </SectionHeader>
       <SectionDescription>
         Recommendations from talented developers with whom I have had the
-        privilege to collaborate
+        privilege of collaborating
       </SectionDescription>
       <RecommendationSwiper
         recommendations={recommedations}
-        recommedationIndex={recommedationIndex}
+        recommendationIndex={recommedationIndex}
+        setRecommedationIndex={setRecommedationIndex}
+      />
+      <RecommendationControl
+        recommendationCount={recommedations.length}
+        recommendationIndex={recommedationIndex}
         setRecommedationIndex={setRecommedationIndex}
       />
     </Section>
   );
 }
-
--- app/components/sections/recommendation/props.ts
+++ app/components/sections/recommendation/props.ts
@@ -1,12 +1,20 @@
 import { Recommendation } from "@/app/data/recommendation"
+import { SpringValue } from "@react-spring/web";
 import { Dispatch, SetStateAction } from "react";
 
 export type RecommendationSwiperProps = {
   recommendations: Array<Recommendation>;
-  recommedationIndex: number;
+  recommendationIndex: number;
   setRecommedationIndex: Dispatch<SetStateAction<number>>;
 }
 
 export type RecommendationCardProps = {
   recommendation: Recommendation;
+  cardTransition: Record<string, SpringValue<number>>;
+}
+
+export type RecommendationControlProps = {
+  recommendationCount: number;
+  recommendationIndex: number;
+  setRecommedationIndex: Dispatch<SetStateAction<number>>;
 }
