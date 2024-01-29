1e4cf58d8fbd6c389cebdb97808a22c0fd8c4a58
Charles <okoyecharles509@gmail.com>
2024-01-29 18:57:14 +0100

    feat: add variants to link northwest icon

--- app/components/svg/abstract/NorthWestIcon.tsx
+++ app/components/svg/abstract/NorthWestIcon.tsx
@@ -1,4 +1,8 @@
-export default function NorthWestIcon() {
+export default function NorthWestIcon({
+  variant = "default",
+}: {
+  variant?: "default" | "link";
+}) {
   return (
     <svg
       width="8"
@@ -6,7 +10,11 @@ export default function NorthWestIcon() {
       viewBox="0 0 8 8"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className="stroke-grey-5 dark:stroke-grey-6 "
+      className={
+        variant === "default"
+          ? "stroke-grey-5 dark:stroke-grey-6"
+          : "stroke-blue-200 dark:stroke-blue-d-200"
+      }
     >
       <path
         d="M1 6.65685L6.65685 1M6.65685 1H2.41421M6.65685 1V5.24264"
