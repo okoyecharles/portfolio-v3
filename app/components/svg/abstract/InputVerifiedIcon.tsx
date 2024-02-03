ba2a3b1d25ed3145bc4d3014edc70780385ebbcc
Charles <okoyecharles509@gmail.com>
2024-02-03 19:38:17 +0100

    feat: add icons for contact section

---
+++ app/components/svg/abstract/InputVerifiedIcon.tsx
@@ -0,0 +1,18 @@
+export default function InputVerifiedIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      xmlns="http://www.w3.org/2000/svg"
+      className="stroke-blue-200 dark:stroke-blue-d-200"
+    >
+      <path
+        d="M13 2L5 9.99964L1 6"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
