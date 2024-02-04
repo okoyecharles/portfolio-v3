f98632b2ad13c941fd1e929eba19f57130b2f8d2
Charles <okoyecharles509@gmail.com>
2024-02-04 19:27:06 +0100

    feat: add form validation functions

---
+++ app/components/utils/validation.ts
@@ -0,0 +1,63 @@
+export type FormValidationError = {
+  message: string;
+  field: 'name' | 'email' | 'message';
+}
+
+export type FormValidation = {
+  success: boolean;
+  error: FormValidationError | null;
+}
+
+const defaultValidation: FormValidation = {
+  error: null,
+  success: true
+};
+
+function assignError(
+  validation: FormValidation,
+  errorField: FormValidationError['field'],
+  errorMessage: FormValidationError['message']
+): void {
+  validation.success = false;
+  validation.error = {
+    message: errorMessage,
+    field: errorField
+  }
+}
+
+export function validateName(name: string): FormValidation {
+  const returnValidation = Object.assign({}, defaultValidation);
+  return returnValidation;
+}
+
+export function validateEmail(email: string): FormValidation {
+  const returnValidation = Object.assign({}, defaultValidation);
+  const EMAIL_REGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (!EMAIL_REGEX.test(email)) {
+    assignError(returnValidation, 'email', 'Please enter a valid email');
+    return returnValidation;
+  }
+
+  return returnValidation;
+}
+
+export async function verifyEmail(email: string): Promise<FormValidation> {
+  const returnValidation = Object.assign({}, defaultValidation);
+  return returnValidation;
+}
+
+export function validateMessage(message: string): FormValidation {
+  const returnValidation = Object.assign({}, defaultValidation);
+  const MIN_MESSAGE_LENGTH = 15;
+
+  if (message.length < MIN_MESSAGE_LENGTH) {
+    assignError(
+      returnValidation,
+      'message',
+      `Oops! Message cannot be less than ${MIN_MESSAGE_LENGTH} characters`
+    );
+  }
+
+  return returnValidation;
+}
