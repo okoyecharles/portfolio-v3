5896b9d10d03d752d12d5405bbd4ddd028be49b6
Charles <okoyecharles509@gmail.com>
2024-03-19 19:43:01 +0100

    feat: finalize  accessibility for experience section

--- app/components/sections/experience/Experience.tsx
+++ app/components/sections/experience/Experience.tsx
@@ -8,19 +8,19 @@ import ExperienceControl from "./ExperienceControl";
 import ExperienceCard from "./ExperienceCard";
 import ExperienceTimeline from "./ExperienceTimeline";
 import { useInView } from "react-intersection-observer";
-import experienceData, { experienceTimelineCalculator, } from "@/app/data/experience";
-import { useSpring, useSpringRef, useTrail, useTransition, } from "@react-spring/web";
+import experienceData, { experienceTimelineCalculator } from "@/app/data/experience";
+import { useSpring, useSpringRef, useTrail, useTransition } from "@react-spring/web";
 
 export default function Experience() {
   const [expertiseIndex, setExpertiseIndex] = useState<number>(0);
   const expertise = experienceData.expertise[expertiseIndex];
-  const {ref, inView} = useInView({
+  const { ref, inView } = useInView({
     threshold: 0,
     rootMargin: "0px 0px -512px",
   });
   const [viewed, setViewed] = useState<boolean>(false);
 
-  const {YEAR_TIMELINE_POS, MONTH_TIMELINE_HEIGHT} =
+  const { YEAR_TIMELINE_POS, MONTH_TIMELINE_HEIGHT } =
     experienceTimelineCalculator(expertise);
   useEffect(() => {
     if (inView && !viewed) {
@@ -32,20 +32,20 @@ export default function Experience() {
       // image animations
       imageTransRef.start();
       // card animations
-      CRApi.set({y: 32, opacity: 0});
+      CRApi.set({ y: 32, opacity: 0 });
       CRApi.start({
         y: 0,
         opacity: 1,
         delay: 500,
-        config: {tension: 400, friction: 40},
+        config: { tension: 400, friction: 40 },
       });
       // timeline animations
-      YTSApi.update({y: -YEAR_TIMELINE_POS});
+      YTSApi.update({ y: -YEAR_TIMELINE_POS });
       YTSApi.start();
-      MTHApi.update({height: MONTH_TIMELINE_HEIGHT});
+      MTHApi.update({ height: MONTH_TIMELINE_HEIGHT });
       MTHApi.start();
-      MTMApi.set({opacity: 0});
-      MTMApi.start({opacity: 1});
+      MTMApi.set({ opacity: 0 });
+      MTMApi.start({ opacity: 1 });
     }
   }, [viewed, expertiseIndex]);
 
@@ -54,7 +54,7 @@ export default function Experience() {
   const imageTransition = useTransition(expertiseIndex, {
     ref: imageTransRef,
     keys: null,
-    from: {opacity: 0, rotateX: 0, rotateY: 0, rotateZ: 0, y: "-40%"},
+    from: { opacity: 0, rotateX: 0, rotateY: 0, rotateZ: 0, y: "-40%" },
     enter: {
       opacity: 1,
       rotateX: 4,
@@ -69,7 +69,7 @@ export default function Experience() {
       rotateY: -48,
       rotateZ: 5,
       y: "-50%",
-      config: {tension: 350},
+      config: { tension: 350 },
     },
   });
 
@@ -77,7 +77,7 @@ export default function Experience() {
   const [contentReveal, CRApi] = useTrail(
     4,
     {
-      from: {y: 32, opacity: 0},
+      from: { y: 32, opacity: 0 },
     },
     []
   );
@@ -85,14 +85,14 @@ export default function Experience() {
   // Timeline Animations
   const [yearTimeLineScroll, YTSApi] = useSpring(
     () => ({
-      from: {y: -275},
+      from: { y: -275 },
     }),
     []
   );
 
   const [monthTimeLineHeight, MTHApi] = useSpring(
     () => ({
-      from: {height: 0},
+      from: { height: 0 },
     }),
     []
   );
@@ -109,27 +109,32 @@ export default function Experience() {
       padding="pt-12 pb-16 md:pt-8 md:pb-[224px]"
     >
       <SectionHeader mode="standalone">
-        My <span className="text-blue-100 dark:blue-d-200">experience</span> as
-        a developer
+        My <span className="text-blue-100 dark:blue-d-200">experience</span> as a
+        developer
       </SectionHeader>
       <SectionDescription>
-        A display of my growth as a frontend developer, showcasing the progress
-        I have achieved and the valuable experience I've acquired
+        A display of my growth as a frontend developer, showcasing the progress I have
+        achieved and the valuable experience I've acquired
       </SectionDescription>
-      <div
-        className="relative flex experience-content"
-        style={{perspective: "800px"}}
-      >
-        <ExperienceTimeline
-          expertise={expertise}
-          yearTimeLineScroll={yearTimeLineScroll}
-          monthTimeLineHeight={monthTimeLineHeight}
-          monthTimeLineMarker={monthTimeLineMarker}
-        />
-        <ExperienceCard expertise={expertise} contentReveal={contentReveal} />
-        <ExperienceImage imageTransition={imageTransition} />
+      <div aria-label="experience carousel">
+        <div
+          className="relative flex experience-content"
+          id={`experience-item-${expertiseIndex + 1}`}
+          role="tabpanel"
+          style={{ perspective: "800px" }}
+        >
+          <ExperienceTimeline
+            expertise={expertise}
+            yearTimeLineScroll={yearTimeLineScroll}
+            monthTimeLineHeight={monthTimeLineHeight}
+            monthTimeLineMarker={monthTimeLineMarker}
+          />
+          <ExperienceCard expertise={expertise} contentReveal={contentReveal} />
+          <ExperienceImage imageTransition={imageTransition} />
+        </div>
       </div>
       <ExperienceControl
+        expertiseData={experienceData.expertise}
         expertiseIndex={expertiseIndex}
         setExpertiseIndex={setExpertiseIndex}
         expertiseCount={experienceData.expertise.length}
@@ -137,4 +142,3 @@ export default function Experience() {
     </Section>
   );
 }
-
