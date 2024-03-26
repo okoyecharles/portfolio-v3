dfcf4cdab362950c4e4036d6e170c61493765d8c
Charles <okoyecharles509@gmail.com>
2024-03-26 16:03:21 +0100

    refactor: add extra functionality to button component

--- app/components/clickable/Button.tsx
+++ app/components/clickable/Button.tsx
@@ -1,6 +1,6 @@
-import { DOMAttributes } from "react";
+import { DOMAttributes, KeyboardEvent, useRef } from "react";
 
-const className = {
+const buttonStyles = {
   default: `
     group/button button
     select-none px-4 py-[10px] rounded-[5px]
@@ -16,7 +16,7 @@ const className = {
     disabled:opacity-70 disabled:hover:cursor-not-allowed
   `,
   variant: {
-    default: `
+    blue: `
       bg-blue-200 text-white fill-white hover:bg-blue-300
       dark:bg-blue-d-300 dark:hover:bg-blue-d-300/90
       disabled:bg-blue-200 dark:disabled:bg-blue-d-300
@@ -26,35 +26,51 @@ const className = {
       dark:bg-black dark:ring-1 dark:ring-grey-5 dark:hover:bg-grey-15 dark:hover:ring-grey-6 dark:active:bg-black
       disabled:bg-grey-1
     `,
+    plain: ``
   },
 };
 
 interface ButtonProps {
   children: React.ReactNode;
   disabled?: boolean;
-  variant?: keyof (typeof className)["variant"];
+  variant?: keyof (typeof buttonStyles)["variant"];
   tabIndex?: number;
+  className?: string;
   onClick?: DOMAttributes<HTMLButtonElement>["onClick"];
+  ariaLabel?: string;
 }
 
 function Button({
   disabled,
   children,
-  variant = "default",
+  variant = "plain",
   tabIndex,
+  className,
   onClick,
+  ariaLabel
 }: ButtonProps) {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  function handleKeyDown(event: KeyboardEvent<HTMLButtonElement>) {
+    if (event.key === "Enter") {
+      buttonRef.current?.click();
+    }
+  }
+
   return (
     <button
+      ref={buttonRef}
       className={`
-        ${className.default}
-        ${className.focus}
-        ${className.disabled}
-        ${className.variant[variant]}
+        ${variant !== "plain" ? buttonStyles.default : ''}
+        ${buttonStyles.focus}
+        ${buttonStyles.disabled}
+        ${buttonStyles.variant[variant]}
+        ${className}
       `}
       tabIndex={tabIndex}
       onClick={onClick}
+      onKeyDown={handleKeyDown}
       disabled={disabled}
+      aria-label={ariaLabel}
     >
       {children}
     </button>
