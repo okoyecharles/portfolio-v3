208fa8376ece303b3fe3d85bca6c1def78837575
Charles <okoyecharles509@gmail.com>
2024-04-29 18:42:08 +0100

    feat: add google analytics scripts

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,32 @@ import Projects from "./components/sections/project/Projects";
 import Recommendations from "./components/sections/recommendation/Recommendations";
 import Contact from "./components/sections/contact/Contact";
 import SkipToContent from "./components/sections/skip-to-content/SkipToContent";
+import Script from "next/script";
 
 export default function Page() {
   return (
     <>
+      {/* analytics tags begin here */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TAG_ID}`}
+        
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TAG_ID}');
+          `
+        }}
+      />
+      {/* analytics tags ends here */}
+
       <SkipToContent />
       <Navigation />
       <Home />
